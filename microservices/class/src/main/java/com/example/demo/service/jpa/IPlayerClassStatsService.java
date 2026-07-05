b2bf43337a350b4d54fe8be9514e7d43add83436
UlisesChoco <ulisesjustosaucedo@gmail.com>
2026-07-05 16:47:22 -0300

    Fix file tree

--- microservices/class/src/main/java/com/example/demo/service/jpa/IPlayerClassStatsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.jpa;
-
-import com.example.demo.dto.BonusStatsDTO;
-import com.example.demo.dto.PlayerStatsDTO;
-import com.example.demo.exception.InvalidPlayerClassDataException;
-
-public interface IPlayerClassStatsService {
-    PlayerStatsDTO getScaledPlayerClassStats(Long classId, BonusStatsDTO bonus, int level) throws InvalidPlayerClassDataException;
-}
