ac0d83f606da1e653fb837a1a3905d1f1cb0c388
UlisesChoco <ulisesjustosaucedo@gmail.com>
2026-09-19 16:21:57 -0300

    Monolith initial state

---
+++ backend/src/main/kotlin/com/chocolatada/web_mmorpg/WebMmorpgApplication.kt
@@ -0,0 +1,11 @@
+package com.chocolatada.web_mmorpg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebMmorpgApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebMmorpgApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/chocolatada/web_mmorpg/WebMmorpgApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chocolatada.web_mmorpg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebMmorpgApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
